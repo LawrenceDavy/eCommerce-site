2617b6bc7181082b32a44b83ac77dc6262b63b35
Lawrence Davy <aiwass@Lawrences-MBP.home>
2017-08-15 20:21:09 +0100

    Initial commit

---
+++ src/checkout/views.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.conf import settings
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+import stripe
+
+stripe.api_key = settings.STRIPE_SECRET_KEY
+
+# Create your views here.
+@login_required
+def checkout(request):
+	publishKey = settings.STRIPE_PUBLISHABLE_KEY
+	customer_id = request.user.userstripe.stripe_id
+	if request.method == 'POST':
+		token = request.POST['stripeToken']
+		# Create a charge: this will charge the user's card
+		try:
+			customer = stripe.Customer.retrieve(customer_id)
+			customer.sources.create(source=token)
+			charge = stripe.Charge.create(
+				amount=1000,
+				currency="gbp",
+				description="Example charge",
+				customer = customer,
+			)
+		except stripe.error.CardError as e:
+			# The card has been declined
+			pass
+	context = {'publishKey': publishKey}
+	template = 'checkout.html'
+	return render(request,template, context)
